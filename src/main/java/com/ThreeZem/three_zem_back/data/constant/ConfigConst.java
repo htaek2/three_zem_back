64d2f581507d6c88f43a85be74c2664cd39b3519
htkim <gusxo909@gmail.com>
2025-10-22 12:49:08 +0900

    실시간 데이터 업데이트 시간 변경

--- src/main/java/com/ThreeZem/three_zem_back/data/constant/ConfigConst.java
+++ src/main/java/com/ThreeZem/three_zem_back/data/constant/ConfigConst.java
@@ -2,7 +2,7 @@ package com.ThreeZem.three_zem_back.data.constant;
 
 public class ConfigConst {
 
-    public static final int DATA_UPDATE_MS = 10000;
+    public static final int DATA_UPDATE_MS = 5000;
     public static final int DATA_SAVE_MS = 600000;
 
 }
